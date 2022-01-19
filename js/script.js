77f859bcef952dfdb6248be1a91c265f667afab5
Kester Tan <kester@Kesters-MacBook-Pro.local>
2022-01-20 02:39:22 +0800

    added photos html

--- js/script.js
+++ js/script.js
@@ -16,10 +16,47 @@ const navSlide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.4}s`;
             }
         });
-
         // Burger Animation
         burger.classList.toggle(`toggle`);
     });
+}
+
+// Gallery
+const galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+const windowWidth = window.innerWidth;
+
+// For each gallery image, open image up
+if (galleryImages) {
+    galleryImages.forEach((image, index) => {
+        image.onclick = () => {
+            // alert("working");
+            // Get background image url
+            const getElement = window.getComputedStyle(image);
+            const FullImgUrl = getElement.getPropertyValue("background-image");
+            // alert(FullImgUrl);
+            const ImgUrlPos = FullImgUrl.split("/images/");
+            const setNewImgUrl = ImgUrlPos[1].replace('")', '');
+            // alert(setNewImgUrl);
+
+            getLatestOpenedImg = index + 1;
+
+            // Create div container for image being brought up
+            const container = document.body;
+            const newImgWindow = document.createElement("div");
+            container.appendChild(newImgWindow);
+            newImgWindow.setAttribute("class", "img-window");
+            newImgWindow.setAttribute("onclick", "closeImg()");
+
+            const newImg = document.createElement("img");
+            newImgWindow.appendChild(newImg);
+            newImg.setAttribute("src", "images/" + setNewImgUrl);
+        }
+    });
+}
+
+function closeImg() {
+    document.querySelector(".img-window").remove();
 }
 
 navSlide();
